3f69ca004e7ae98610dfccf45941f50f82356377
Emmanuel Akinrintoyo <emmanueljones2090@gmail.com>
2018-09-28 23:15:55 +0100

    Add files via upload

---
+++ var.c
@@ -0,0 +1,12 @@
+//Author: Emmanuel Akinrintoyo
+//Date: 12/09/2018
+
+#include <stdio.h>
+void main()
+{
+char MyVariable; // Line 3
+MyVariable = 29; // Line 4
+MyVariable = MyVariable+1;
+printf("The decimal value of MyVariable is : %u\n",MyVariable);
+printf("The hexadecimal value of MyVariable is : %X\n",MyVariable);
+}
