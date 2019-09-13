a3becde48bb463df7d5517f5e1735b389b10e834
Emmanuel Akinrintoyo <emmanueljones2090@gmail.com>
2019-09-13 01:05:53 +0100

    Update ops.c

--- ops.c
+++ ops.c
@@ -3,22 +3,22 @@
 
  #include <stdio.h>
  int main(void) {
- printf("5 + 4 = %d\n", 5 + 4); //Addition
- printf("5 - 4 = %d\n", 5 - 4); //Sub
- printf("5 * 4 = %d\n", 5 * 4); //Mulitply
- printf("(float)5 / (float)4 = %f\n",(float)5 / (float)4);
- printf("5 / 4 = %d\n",5 / 4);
- printf("5 % 4 = %d\n",5 % 4);//MODULO	
- printf("5 & 4 = %d\n",5 & 4);//BITWISE AND
- printf("5 | 4 = %d\n",5 | 4);//BITWISE OR
- printf("5 ^ 4 = %d\n",5 ^ 4);//BITWISE XOR
- printf("~5 = (hex) %x\n", ~5);//BITWISE COMPLEMENT OR BITWISE NOT
- 
- printf("Quiz1 %d\n", 0x1389 & 0x2821);
- printf("Quiz2 %d\n", ~0x8378);
-printf("Quiz3 %d\n", 1234 | 5678);
-printf("Quiz4 %d\n", 0xa5a5 ^ 0xffff);
-printf("Quiz5 %d\n", 128 % 11);
+  
+  printf("5 + 4 = %d\n", 5 + 4); //Addition
+  printf("5 - 4 = %d\n", 5 - 4); //Sub
+  printf("5 * 4 = %d\n", 5 * 4); //Mulitply
+  printf("(float)5 / (float)4 = %f\n",(float)5 / (float)4);
+  printf("5 / 4 = %d\n",5 / 4);
+  printf("5 % 4 = %d\n",5 % 4);//MODULO	
+  printf("5 & 4 = %d\n",5 & 4);//BITWISE AND
+  printf("5 | 4 = %d\n",5 | 4);//BITWISE OR
+  printf("5 ^ 4 = %d\n",5 ^ 4);//BITWISE XOR
+  printf("~5 = (hex) %x\n", ~5);//BITWISE COMPLEMENT OR BITWISE NOT
+  printf("Quiz1 %d\n", 0x1389 & 0x2821);
+  printf("Quiz2 %d\n", ~0x8378);
+  printf("Quiz3 %d\n", 1234 | 5678);
+  printf("Quiz4 %d\n", 0xa5a5 ^ 0xffff);
+  printf("Quiz5 %d\n", 128 % 11);
 
 
 }
