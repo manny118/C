15c5320c8e7a6ccdda362303aaee56ac06a0d652
Emmanuel Akinrintoyo <emmanueljones2090@gmail.com>
2019-09-13 01:06:45 +0100

    Update var.c

--- var.c
+++ var.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 void main()
 {
-char MyVariable; // Line 3
-MyVariable = 29; // Line 4
-MyVariable = MyVariable+1;
-printf("The decimal value of MyVariable is : %u\n",MyVariable);
-printf("The hexadecimal value of MyVariable is : %X\n",MyVariable);
+  char MyVariable; // Line 3
+  MyVariable = 29; // Line 4
+  MyVariable = MyVariable+1;
+  printf("The decimal value of MyVariable is : %u\n",MyVariable);
+  printf("The hexadecimal value of MyVariable is : %X\n",MyVariable);
 }
